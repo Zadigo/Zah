07280aaac8bbb3504de31b0888707ad9a2943964
John Pendenque <pendenquejohn@gmail.com>
2021-07-12 11:07:12 +0200

    Documentation

--- config.py
+++ config.py
@@ -3,10 +3,30 @@ import os
 from jinja2.environment import Environment
 from jinja2.loaders import FileSystemLoader
 
+# The base path for your project
+
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+# The default location of the static folder
+# for your application
 
 STATIC_ROOT = os.path.join(BASE_PATH, 'static')
 
+# The default location of the templates
+# folder for your application
+
 TEMPLATES = os.path.join(BASE_PATH, 'templates')
 
+# This is the default template backend where
+# Zah searches and serves the different
+# templates
+
 TEMPLATE_BACKEND = Environment(loader=FileSystemLoader(TEMPLATES), autoescape=True)
+
+# Use this to indicate where the frontend
+# app is located so that Jinja can serve
+# the index.html file of the VueJS
+# application
+
+FRONT_END_ROOT = None
