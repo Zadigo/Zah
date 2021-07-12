9c9502b1bc306f5188fa8118cb56068dfd483704
John Pendenque <pendenquejohn@gmail.com>
2021-07-12 11:07:04 +0200

    Ability to get different router instances

--- router.py
+++ router.py
@@ -1,7 +1,5 @@
 from collections import deque
-from typing import Callable, Dict, List, Tuple, Union
-
-from zah.urls import render
+from typing import Callable, List, Tuple, Type, Union
 
 
 class Router:
@@ -13,6 +11,13 @@ class Router:
         # self.routes.append(base_api_route)
         return self.routes
 
+    @classmethod
+    def copy(cls):
+        routes = cls.routes.copy()
+        instance = cls()
+        instance.routes = routes
+        return instance
+
     def add_route(self, path: str, view: Callable, name: str = None):
         """Add a single route to the router"""
         self.routes.append({ 'path': path, 'name': name, 'view': view })
@@ -20,10 +25,13 @@ class Router:
 
     def add_url_patterns(self, urls: Union[list, tuple]):
         """Add multiple routes at once to the router"""
-        pass
+        for url in urls:
+            if not callable(url):
+                raise TypeError('Url should be a callable')
+            self.add_route(**url)
 
     def match(self, path: str, route_name: str = None) -> Tuple[dict[str, str, Callable], List[dict]]:
-        """Match a path or a route name to the registered urls"""
+        """Get a url using a path and enventually a router name"""
         def matcher(route_dict: dict):
             registered_path = route_dict.get('path', None)
             if registered_path is not None:
@@ -45,3 +53,15 @@ class Router:
         if len(candidates) >= 1:
             return candidates[0], candidates
         return [], candidates
+
+    # def match_from_name(self, route_name: str) -> Tuple[dict[str, str, Callable], List[dict]]:
+    #     pass
+
+
+def reverse_url(name: str) -> Union[str, None]:
+    # TODO:
+    router = Router.copy()
+    candidate, _ = router.match(route_name=name)
+    if not candidate:
+        return None
+    return candidate['path']
